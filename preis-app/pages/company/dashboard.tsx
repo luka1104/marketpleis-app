ce6fbdfd893c28f7cf459d07d602ee5a7dccb399
luka1104 <skyward.luke11@gmail.com>
2022-09-10 12:14:56 +0900

    feat: show posted items

--- preis-app/pages/company/dashboard.tsx
+++ preis-app/pages/company/dashboard.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import Navbar from '../../src/components/navbar'
 import AddItems from '../../src/components/company/addItems'
+import PostedItem from '../../src/components/company/postedItem'
 import type { GetServerSideProps } from "next";
 import prisma from '../../lib/prisma'
 import { UserContext } from '../../src/contexts/userContext';
@@ -8,14 +9,18 @@ import { useSession } from "../../sdk/next-auth/react"
 
 type Props = {
   users: any;
+  items: any;
 };
 export const getServerSideProps: GetServerSideProps<Props> = async () => {
   const usersRaw = await prisma.user.findMany()
   const users = JSON.parse(JSON.stringify(usersRaw))
   console.log(users);
+  const itemsRaw = await prisma.item.findMany()
+  const items = JSON.parse(JSON.stringify(itemsRaw))
   return {
     props: {
       users,
+      items,
     },
   };
 }
@@ -27,17 +32,46 @@ const Dashboard = (props: any) => {
   if(status) {
     //@ts-ignore
     const user = props.users.find(u => u.email === session?.user?.email)
-    console.log(props.users);
-    console.log(user);
+    // console.log(props.users);
+    //@ts-ignore
+    const items = props.items.filter(i => i.userId === session?.user?.id)
+    // console.log(items);
+    // console.log(session);
+    // console.log(props.items);
     if(!currentUser) {
       setCurrentUser(user);
     }
+    return (
+      <div>
+        <Navbar />
+        dashboard
+        <AddItems />
+        {//@ts-ignore
+        items.map((val: any, key: any) => {
+          return (
+            <PostedItem
+              item={val}
+              key={key}
+            />
+          )
+        })}
+      </div>
+    )
   }
   return (
     <div>
       <Navbar />
       dashboard
       <AddItems />
+      {/* {//@ts-ignore
+      items.map((val: any, key: any) => {
+        return (
+          <PostedItem
+            item={val}
+            key={key}
+          />
+        )
+      })} */}
     </div>
   )
 }
