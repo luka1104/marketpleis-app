2b65b4ab4871090e3dc4a43df9af65d0d3f531eb
luka1104 <skyward.luke11@gmail.com>
2022-09-12 10:46:32 +0900

    feat: success minting

--- pleis-contract/hardhat.config.js
+++ pleis-contract/hardhat.config.js
@@ -1,6 +1,17 @@
-require("@nomicfoundation/hardhat-toolbox");
-
 /** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.9",
-};
+
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+const { API_URL, PRIVATE_KEY } = process.env;
+ module.exports = {
+    solidity: "0.8.9",
+    defaultNetwork: "mumbai",
+    networks: {
+      hardhat: {},
+      mumbai: {
+        url: API_URL,
+        accounts: [`0x${PRIVATE_KEY}`],
+      },
+    },
+ };
