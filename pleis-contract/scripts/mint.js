ad71a93b576903cb275cd44cbf25faf3ace70e39
luka1104 <skyward.luke11@gmail.com>
2022-09-12 10:45:59 +0900

    feat: create mint test script

---
+++ pleis-contract/scripts/mint.js
@@ -0,0 +1,58 @@
+require("dotenv").config()
+const API_URL = process.env.API_URL
+const PUBLIC_KEY = process.env.PUBLIC_KEY
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3")
+const web3 = createAlchemyWeb3(API_URL)
+
+const contract = require("../artifacts/contracts/RightTicket.sol/RightTicket.json")
+const contractAddress = "0x98Dc4BdEB2b8Ac97bB58E1baF4A6B01536B080fD"
+const nftContract = new web3.eth.Contract(contract.abi, contractAddress)
+
+async function mintNFT(tokenURI) {
+  const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, "latest") //get latest nonce
+
+  //the transaction
+  const tx = {
+    from: PUBLIC_KEY,
+    to: contractAddress,
+    nonce: nonce,
+    gas: 500000,
+    data: nftContract.methods.mintNFT("0x50B80aa3877fC852f3194a0331177FDDcF0891bf", tokenURI).encodeABI(),
+  }
+
+  const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY)
+  signPromise
+    .then((signedTx) => {
+      web3.eth.sendSignedTransaction(
+        signedTx.rawTransaction,
+        function (err, hash) {
+          if (!err) {
+            console.log(
+              "The hash of your transaction is: ",
+              hash,
+              "\nCheck Alchemy's Mempool to view the status of your transaction!"
+            )
+          } else {
+            console.log(
+              "Something went wrong when submitting your transaction:",
+              err
+            )
+          }
+        }
+      )
+    })
+    .catch((err) => {
+      console.log("Promise failed:", err)
+    })
+}
+
+mintNFT(
+  {
+    "name": "The right ticket for purchasing PS5",
+    "title": "PS5",
+    "price": "55000",
+    "holderName": "Pleis Tarou"
+  }
+)
